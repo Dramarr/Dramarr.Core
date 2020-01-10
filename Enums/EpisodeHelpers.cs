033b19b6b3eb6f2dee77b40fb3c92f543b5d8dc8
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2020-01-10 12:03:14 +0000

    Publish nuget bat

---
+++ Enums/EpisodeHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dramarr.Core.Enums
+{
+    public class EnumsHelpers
+    {
+        public enum EpisodeStatus
+        {
+            UNKNOWN,
+            SCRAPED,
+            NOT_DOWNLOADED,
+            DOWNLOADED,
+            FAILED,
+        }
+    }
+}
---
+++ Enums/SourceHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dramarr.Core.Enums
+{
+    public class SourceHelpers
+    {
+        public enum Source
+        {
+            MYASIANTV,
+            ESTRENOSDORAMAS,
+            KSHOW
+        }
+    }
+}
