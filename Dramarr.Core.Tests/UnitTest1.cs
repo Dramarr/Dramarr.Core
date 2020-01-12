f294d7d75f72df16860691b37e18a4b00dda5776
Emiliano Montesdeoca del Puerto <emimontesdeoca@outlook.com>
2020-01-12 00:26:00 +0000

    Updated travis, project structure and added test project

---
+++ Dramarr.Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dramarr.Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
