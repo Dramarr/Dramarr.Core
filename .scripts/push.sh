10bc135a7fc3a738e952d0c95722551827a5b36b
Emiliano Montesdeoca del Puerto <emimontesdeoca@outlook.com>
2020-01-16 21:58:18 +0000

    New update, changed script and travis rollback

--- .scripts/push.sh
+++
@@ -1,2 +0,0 @@
-# dotnet nuget push ./Dramarr.Core/bin/Release/*.nupkg -s "https://nuget.org"
-dotnet nuget push ./Dramarr.Core/bin/Release/*.nupkg -s "https://nuget.pkg.github.com/Dramarr/index.json"
